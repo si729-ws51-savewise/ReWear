95a6ea2fccd849532a7d2647f2be34aa46acfb35
Daniel <U202121440@upc.edu.pe>
2023-09-28 17:49:46 -0500

    feat(add-product): added component for post products

---
+++ ReWear/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from "@angular/common/http";
+import {StudentsService} from "./learning/services/students.service";
+import {StudentsComponent} from './learning/pages/students/students.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatTableModule} from "@angular/material/table";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatCardModule} from "@angular/material/card";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatTabsModule} from '@angular/material/tabs';
+import { RetailersComponent } from '/pages/retailers/retailers/retailers.component';
+import {MatSelectModule} from '@angular/material/select';
+
+@NgModule({
+    declarations: [AppComponent, StudentsComponent, RetailersComponent],
+    imports: [BrowserModule, MatSelectModule,AppRoutingModule, BrowserAnimationsModule, HttpClientModule, MatTabsModule,FormsModule, ReactiveFormsModule, MatTableModule, MatPaginatorModule, MatInputModule, MatButtonModule, MatFormFieldModule, MatToolbarModule, MatIconModule, MatCardModule, MatGridListModule, MatDividerModule],
+    providers: [StudentsService],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
